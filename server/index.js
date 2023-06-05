ad3a6a2650efb3a0e2fa9b023bb03703c0f17573
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-06-05 23:38:59 +0530

    connected database added dependencies

--- server/index.js
+++ server/index.js
@@ -1,9 +1,30 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import compression from 'compression';
+import userRoutes from './src/routes/user.js';
+
+dotenv.config();
+
+const corsConfig = {
+  credentials: 'true',
+  origin: 'http://localhost:3000',
+  optionSuccessStatus: '200',
+};
 
 const app = express();
+app.use(compression());
+app.use(cors(corsConfig));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
+
+app.use('/user', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=> {
+mongoose.connect(process.env.DATABASE_URL)
+  .then(() => app.listen(PORT, () => {
     console.log(`Trackminster server is running on port ${PORT}`);
-})
+  }))
+  .catch((err) => console.log(err));
