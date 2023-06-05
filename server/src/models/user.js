1d99f2a99885dd3a0d1b65cf7dec8f6af92c6504
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-06-05 23:39:29 +0530

    made a user model

---
+++ server/src/models/user.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  contact: {
+    type: String,
+    default: '',
+  },
+  department: {
+    type: String,
+    default: '',
+  },
+  doj: {
+    type: String,
+    default: '',
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model('User', userSchema);
