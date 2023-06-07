ae5dd51c036fb19b0d479ff08c8665b9016a24ca
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-06-08 02:31:12 +0530

    added redux

---
+++ client/src/redux/reducers/auth.js
@@ -0,0 +1,19 @@
+import * as actionType from "../const/actionTypes";
+
+// eslint-disable-next-line default-param-last
+const authReducer = (state = { authData: null }, action) => {
+  switch (action.type) {
+    case actionType.AUTH:
+      localStorage.setItem("user_info", JSON.stringify({ ...action?.data }));
+      console.log(action.data);
+      return { ...state, authData: action.data };
+    case actionType.LOGOUT:
+      localStorage.clear();
+
+      return { ...state, authData: null };
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
