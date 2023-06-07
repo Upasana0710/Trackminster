197aad3c5e5606d1abec200caec05d9c7f583033
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-06-07 20:46:44 +0530

    set themes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./utils/Theme";
+import Navbar from "./components/Navbar";
+import Menu from "./components/Menu";
+import Login from "./components/Login";
+
+const Trackminster = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 100vh;
+  background: ${({ theme }) => theme.bgLight};
+  overflow-y: hidden;
+  overflow-x: hidden;
+`;
 
 function App() {
-  return <div className="App" />;
+  const [darkMode, setDarkMode] = useState(true);
+  const [signUpOpen, setSignUpOpen] = useState(false);
+  useEffect(() => {
+    console.log(signUpOpen);
+  });
+  return (
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Trackminster>
+        <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
+        <Navbar setSignUpOpen={setSignUpOpen} />
+        <Login signUpOpen={signUpOpen} setSignUpOpen={setSignUpOpen} />
+      </Trackminster>
+    </ThemeProvider>
+  );
 }
 
 export default App;
