4ca6b0c31f6b0a5560a3b0d125dd3d4b289ee54b
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-06-08 02:30:19 +0530

    added redux

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import authReducer from "./redux/reducers/auth";
 import "./index.css";
 import App from "./App";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = configureStore(
+  { reducer: authReducer },
+  {},
+  composeEnhancers(applyMiddleware(thunk)),
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
