51346546653d039cf666f86422500837bb0b0b41
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-06-09 17:39:13 +0530

    redux updated

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,25 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
-import { applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import authReducer from "./redux/reducers/auth";
+import { store, persistor } from "./redux/store";
 import "./index.css";
 import App from "./App";
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = configureStore(
-  { reducer: authReducer },
-  {},
-  composeEnhancers(applyMiddleware(thunk)),
-);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 );
