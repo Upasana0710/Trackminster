ecf3ab21a05203fa27e93cc739fe602c7c4049de
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-06-09 17:47:38 +0530

    made default dashboard for admin

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Home = () => {
-  return <div />;
+const Dashboard = () => {
+  return <div>Dashboard</div>;
 };
 
-export default Home;
+export default Dashboard;
