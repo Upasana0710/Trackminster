1cd5514c5575a57db6592d6f9d25ea783b8e764d
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-06-09 17:38:25 +0530

    changed the flow

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import styled, { ThemeProvider } from "styled-components";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
 import { darkTheme, lightTheme } from "./utils/Theme";
 import Navbar from "./components/Navbar";
 import Menu from "./components/Menu";
 import Login from "./components/Login";
 import AddEmployee from "./pages/AddEmployee";
 import Dashboard from "./pages/Dashboard";
+import DashboardEmp from "./pages/DashboardEmp";
 
 const Trackminster = styled.div`
   display: flex;
@@ -17,25 +19,51 @@ const Trackminster = styled.div`
   overflow-y: hidden;
   overflow-x: hidden;
 `;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+  background: ${({ theme }) => theme.bgLight};
+  overflow-y: hidden;
+  overflow-x: hidden;
+`;
 
 function App() {
   const [darkMode, setDarkMode] = useState(true);
-  const [signUpOpen, setSignUpOpen] = useState(false);
+  const { currentUser } = useSelector((state) => state.user);
   useEffect(() => {
-    console.log(signUpOpen);
+    console.log(currentUser);
   });
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <BrowserRouter>
-        <Trackminster>
-          <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
-          <Navbar setSignUpOpen={setSignUpOpen} />
-          <Login signUpOpen={signUpOpen} setSignUpOpen={setSignUpOpen} />
-          <Routes>
-            <Route path="/" exact element={<Dashboard />} />
-            <Route path="/addemployee" exact element={<AddEmployee />} />
-          </Routes>
-        </Trackminster>
+        {currentUser ? (
+          <Trackminster>
+            <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
+            <Container>
+              <Navbar />
+              <Routes>
+                <Route
+                  path="/"
+                  exact
+                  element={
+                    currentUser?.role === "Admin" ? (
+                      <Dashboard />
+                    ) : (
+                      <DashboardEmp />
+                    )
+                  }
+                />
+                <Route path="/addemployee" exact element={<AddEmployee />} />
+              </Routes>
+            </Container>
+          </Trackminster>
+        ) : (
+          <Trackminster>
+            <Login />
+          </Trackminster>
+        )}
       </BrowserRouter>
     </ThemeProvider>
   );
