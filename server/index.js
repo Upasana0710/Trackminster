52c249fa31259a4fa74f14403f03981aee35e207
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-06-14 01:18:22 +0530

    add task route

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import compression from 'compression';
 import userRoutes from './src/routes/user.js';
+import taskRoutes from './src/routes/task.js';
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
 
 app.use('/user', userRoutes);
+app.use('/task', taskRoutes);
 
 const PORT = process.env.PORT || 5000;
 
--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { authenticate } from '../controllers/user.js';
+import { authenticate, getUser } from '../controllers/user.js';
 
 const router = express.Router();
 
 router.post('/auth', authenticate);
+router.get('/:id', getUser);
 
 export default router;
