888c7e56b41d0e7aaf5be3b8c2f9e11cb32b7971
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-06-14 18:45:07 +0530

    piechart to dashboard

--- client/src/pages/DashboardEmp.jsx
+++ client/src/pages/DashboardEmp.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import styled from "styled-components";
+import { useSelector } from "react-redux";
+import Piechart from "../components/Piechart";
 
+const DashContainer = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+`;
 const DashboardEmp = () => {
-  return <div>DashboardEmp</div>;
+  const { currentUser } = useSelector((state) => state.user);
+  const id = currentUser._id;
+  return (
+    <DashContainer>
+      <Piechart day={true} id={id} />
+      <Piechart day={false} id={id} />
+    </DashContainer>
+  );
 };
 
 export default DashboardEmp;
