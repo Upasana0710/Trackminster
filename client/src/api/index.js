f9ad0b4287c26adbd842171e7fb5d4bc8ce31043
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-06-14 18:44:47 +0530

    added piechart api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -5,7 +5,7 @@ const API = axios.create({ baseURL: "http://localhost:5000" });
 // auth
 export const login = (user) => API.post("/user/auth", user);
 
-// task api
+// task creation
 export const createTask = (task, token) =>
   API.post(
     "/task/create",
@@ -13,3 +13,6 @@ export const createTask = (task, token) =>
     { headers: { Authorization: `Bearer ${token}` } },
     { withCredentials: true },
   );
+
+// pie chart
+export const pieData = (data) => API.post("/task/pie", data);
