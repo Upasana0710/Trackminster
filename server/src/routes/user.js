324bb0cb92309915268f3657d8bb0f064b943cbd
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-06-15 18:07:18 +0530

    added route for get employees

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { authenticate, getUser } from '../controllers/user.js';
+import { authenticate, getUser, getEmployees } from '../controllers/user.js';
 
 const router = express.Router();
 
 router.post('/auth', authenticate);
+router.get('/employees', getEmployees);
 router.get('/:id', getUser);
 
 export default router;
