931c61aad1f7a8c0312c040aca50a79e918a5735
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-06-15 18:07:54 +0530

    made profile page

---
+++ client/src/pages/Employee.jsx
@@ -0,0 +1,116 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import { getEmployee } from "../api/index";
+import Piechart from "../components/Piechart";
+import BarGraph from "../components/BarGraph";
+
+const ProfileContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+  align-items: center;
+  justify-content: flex-start;
+  padding-left: 0px;
+  padding-top: 60px;
+`;
+const Heading = styled.div`
+  color: ${({ theme }) => theme.primary};
+  font-size: 24px;
+  font-weight: 550;
+  padding-bottom: 40px;
+  padding-top: 40px;
+  width: 100%;
+  text-align: center;
+`;
+const Table = styled.h1`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+const Row = styled.div`
+  height: 60px;
+  width: 400px;
+  box-sizing: border-box;
+  background: ${({ theme }) => theme.bgLight};
+  border-bottom: 1px solid ${({ theme }) => theme.text_primary};
+  color: ${({ theme }) => theme.text_secondary};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: 500;
+`;
+const Left = styled.div`
+  flex: 0.5;
+  border-right: 1px solid ${({ theme }) => theme.text_primary};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
+const Right = styled.div`
+  flex: 0.5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const PieContainer = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  gap: 60px;
+  padding: 40px 0px;
+`;
+
+const Employee = () => {
+  const { id } = useParams();
+  const [profile, setProfile] = useState();
+  useEffect(() => {
+    const fetchData = async () => {
+      await getEmployee(id).then((res) => {
+        console.log(res);
+        setProfile(res.data);
+      });
+    };
+    fetchData();
+  }, [id]);
+  return (
+    <ProfileContainer>
+      <Heading>{profile?.name}</Heading>
+      <Table>
+        <Row>
+          <Left>Username</Left>
+          <Right>{profile?.username}</Right>
+        </Row>
+        <Row>
+          <Left>Email</Left>
+          <Right>{profile?.email}</Right>
+        </Row>
+        <Row>
+          <Left>Contact</Left>
+          <Right>{profile?.contact}</Right>
+        </Row>
+        <Row>
+          <Left>Department</Left>
+          <Right>{profile?.department}</Right>
+        </Row>
+        <Row style={{ borderBottom: "none" }}>
+          <Left>Date of Joining</Left>
+          <Right>{profile?.doj}</Right>
+        </Row>
+      </Table>
+      <PieContainer>
+        <Piechart day={true} id={id} />
+        <Piechart day={false} id={id} />
+      </PieContainer>
+      <BarGraph id={id} />
+    </ProfileContainer>
+  );
+};
+
+export default Employee;
