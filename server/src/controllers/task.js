606729924bbd3f5a0182d07df6bd8289b30d2aa5
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-06-17 01:20:39 +0530

    api response for bar data changed

--- server/src/controllers/task.js
+++ server/src/controllers/task.js
@@ -57,29 +57,43 @@ export const barData = async (req, res) => {
     const startOfWeek = new Date(currentDate.setDate(currentDate.getDate() - currentDate.getDay()));
     // eslint-disable-next-line max-len
     const endOfWeek = new Date(currentDate.setDate(currentDate.getDate() - currentDate.getDay() + 6));
-    const filteredTasks = employee.tasks.filter((task) => {
-      const taskDate = new Date(task.startTime);
-      return taskDate >= startOfWeek && taskDate <= endOfWeek;
-    });
 
-    // Calculate total time for each task type
-    const timeByType = filteredTasks.reduce((acc, task) => {
-      const { type, time } = task;
-      if (acc[type]) {
-        acc[type] += time;
-      } else {
-        acc[type] = time;
-      }
-      return acc;
-    }, {});
+    const dataByDay = [];
 
-    // Convert accumulated data to the desired response format
-    const responseData = Object.entries(timeByType).map(([type, time]) => ({
-      type,
-      time,
-    }));
+    // Define the task types and their order
+    const taskTypes = ['Work', 'Meeting', 'Break'];
 
-    res.json(responseData);
+    // Iterate through each day of the week
+    for (let date = new Date(startOfWeek); date <= endOfWeek; date.setDate(date.getDate() + 1)) {
+      const currentDayData = [];
+
+      // Filter tasks for the current day
+      const filteredTasks = employee.tasks.filter((task) => {
+        const taskDate = new Date(task.startTime);
+        return taskDate.toISOString().split('T')[0] === date.toISOString().split('T')[0];
+      });
+
+      // Calculate total time for each task type
+      const timeByType = filteredTasks.reduce((acc, task) => {
+        const { type, time } = task;
+        if (acc[type]) {
+          acc[type] += time;
+        } else {
+          acc[type] = time;
+        }
+        return acc;
+      }, {});
+
+      // Push the task data for each type in the specified order
+      taskTypes.forEach((type) => {
+        const time = timeByType[type] || 0;
+        currentDayData.push({ type, time });
+      });
+
+      dataByDay.push(currentDayData);
+    }
+
+    res.json(dataByDay);
   } catch (error) {
     res.status(500).json({ error: 'Internal Server Error' });
   }
